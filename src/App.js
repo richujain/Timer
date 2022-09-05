7e4c137937acc12c251b17734542731609976efe
Richu <richujain1995@gmail.com>
2022-09-05 01:21:48 -0400

    Title Change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MainNavigation from "./components/Navigation/MainNavigation";
 import Timer from "./components/Timer/Timer";
 
 
 function App() {
+  useEffect(() => {
+    document.title = 'Digital Timer : Richu Jain';
+  });
   const [isLight, setIsLight] = useState(true);
   const colorThemeHandler = () => {
     setIsLight((isLight) => !isLight);
